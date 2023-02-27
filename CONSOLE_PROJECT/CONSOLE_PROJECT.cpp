9776f16f9f444f08b89e8006a25dbebbc1bc4b51
Your Name <you@example.com>
2023-02-27 12:31:18 +0700

    Again

--- CONSOLE_PROJECT/CONSOLE_PROJECT.cpp
+++ CONSOLE_PROJECT/CONSOLE_PROJECT.cpp
@@ -2,22 +2,21 @@
 
 int main()
 {
-    int board[size][size]{};
-    int x = 0, y = 0;
-    bool XTurn = true;
-    system("color F0");
-    drawBoard();
-    while (!win(board, x, y) ) {
-       // drawCharacter(board);
-        move(board, x, y, XTurn);
-    }
-    system("cls");
-    system("color 0F");
-    if (win(board, x, y)) {
-        if (XTurn)  cout << "O won!\n";
-        else        cout << "X won!\n";
-    } 
-   // else if (draw(board)) cout << "Draw!\n";
-    return 0;
+	int board[size][size]{};
+	int x = 0, y = 0;
+	bool XTurn = true;
+	system("color F0");
+	drawBoard();
+	while (!win(board, x, y) && !draw(board)) {
+		move(board, x, y, XTurn);
+	}
+	system("cls");
+	system("color 0F");
+	if (win(board, x, y)) {
+		if (XTurn)  cout << "O won!\n";
+		else        cout << "X won!\n";
+	}
+	else cout << "Draw!\n";
+	return 0;
 }
 
